77d983d3c5bdcfcf93b99c0887fbbb6f146675a4
Sourav Banerjee <99480048+souravwithstars@users.noreply.github.com>
2022-04-23 10:18:25 +0530

    Added two team lists

---
+++ teamLists.js
@@ -0,0 +1,6 @@
+const india = ['rahul', 'rohit', 'kohli', 'iyer', 'pant', 'surya', 'jadeja', 'ashwin', 'bhubaneswar', 'shami', 'bumrah'];
+
+const australia = ['werner', 'finch', 'smith', 'labuschagne', 'stoinis', 'head', 'carey', 'green', 'abott', 'behrendorff', 'zampa'];
+
+exports.india = india;
+exports.australia = australia;
